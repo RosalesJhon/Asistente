c0655e6795570ff0c6468c683a97ad092cd3366b
RosalesJhon <yhordiyhom65@gmail.com>
2023-08-16 23:28:15 -0500

    Por fin funcionó xd

--- Bot.py
+++ Bot.py
@@ -2,28 +2,26 @@ import tkinter as tk
 from tkinter import*
 from PIL import Image, ImageTk, ImageDraw, ImageOps
 
-ventana = tk.Tk()
-ventana.geometry('{}x{}+{}+{}'.format(400, 400, 500, 100))
-ventana.resizable(0,0)
-        
-ventana.title("Asistente")
-ventana.config(bg='#fcfcfc')
+def procesar_info(nm):
+    
+    nomb = nm
 
-image2 = Image.open("./descarga.jpg")
-image2 = image2.resize((120, 78))
-photo2 = ImageTk.PhotoImage(image2)
-img_label2 = tk.Label(ventana, image=photo2)
-img_label2.image = photo2  # Evita que la imagen se borre automáticamente
-img_label2.place(x=150, y=5)
+    ventana = tk.Tk()
+    ventana.geometry('{}x{}+{}+{}'.format(400, 400, 500, 100))
+    ventana.resizable(0,0)
+            
+    ventana.title("Asistente")
+    ventana.config(bg='#fcfcfc')
 
-titulo = Label(text="Hola {nm} en nque puedo ayudarte ?")
-titulo.place(x=50,y=130)
-titulo.config(bg='#fcfcfc')
+    image2 = Image.open("./descarga.jpg")
+    image2 = image2.resize((120, 78))
+    photo2 = ImageTk.PhotoImage(image2)
+    img_label2 = tk.Label(ventana, image=photo2)
+    img_label2.image = photo2
+    img_label2.place(x=150, y=5)
 
-nombres = Label(text="NOMBRES")
-nombres.place(x=40,y=100)
-nombres.config(bg='#fcfcfc')
-entrada_nombre = tk.Entry(width=35,highlightbackground = "black", highlightcolor= "blue", highlightthickness=1,justify="center")
-entrada_nombre.place(x=140,y=100)
+    titulo = Label(text=f"Hola {nomb} ¿En qué puedo ayudarte?")
+    titulo.place(x=50,y=130)
+    titulo.config(bg='#fcfcfc')
 
-ventana.mainloop()
+    ventana.mainloop()
--- user.py
+++ user.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from PIL import ImageTk, Image
+from tkinter import messagebox
 
 ventana = tk.Tk()
 ventana.title("Crear cuenta")
@@ -31,11 +32,18 @@ nm_entrada.place(x=320,y=135)
 pwd_entrada = tk.Entry(width=35,highlightbackground = "black", highlightcolor= "blue", highlightthickness=1, show="*",justify="center",font=('Times', 10))
 pwd_entrada.place(x=320,y=205)
 
-password = pwd_entrada.get()
-nm = nm_entrada.get()
+
 def enviar():
-    ventana.destroy()
-    import Bot
+    password = str(pwd_entrada.get())
+    nm = str(nm_entrada.get())
+    
+    if not nm or not password:
+        messagebox.showerror("Error", "Uno de los campos está vacio")
+        return
+    else:
+        ventana.destroy()
+        import Bot
+        Bot.procesar_info(nm)
     
 button = tk.Button(ventana, text="Crear",command= enviar)
 button.place(x=400,y=280)
